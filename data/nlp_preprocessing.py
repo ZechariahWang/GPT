2cb0b08d354830b1f0ae79fecff46f5a24023b40
Zechariah Wang <97078224+ZechariahWang@users.noreply.github.com>
2026-06-08 11:28:20 -0600

    Update NLP preprocessing (NLP 2/4)

--- data/nlp_preprocessing.py
+++ data/nlp_preprocessing.py
@@ -10,8 +10,10 @@ class Solution:
         # 3. Combine positive + negative into one list of tensors
         # 4. Pad shorter sequences with 0s using nn.utils.rnn.pad_sequence(tensors, batch_first=True)
 
+        # positive sentiment first + negative sentiment
         sentences = positive + negative
 
+        # turn all sentences into a set of words, to avoid duplicates
         vocab_words = set()
         for s in sentences:
             words = s.split()
@@ -19,12 +21,15 @@ class Solution:
                 vocab_words.add(w)
         vocab_words = sorted(vocab_words)
 
+        # assign a tokenid to each words (token)
+        # start iterating from 1 since 0 index is considered padding loc
         vocab = {}
         i=1
         for w in vocab_words:
             vocab[w] = i
             i += 1
 
+        # convert each word back into its token id and append to the torch tensor
         seq = []
         for s in sentences:
             words = s.split()
@@ -33,6 +38,7 @@ class Solution:
                 numbers.append(vocab[w])
             seq.append(torch.tensor(numbers))
 
+        # return padding seq
         return torch.nn.utils.rnn.pad_sequence(seq, padding_value=0, batch_first=True)
 
         
